4c5401c0bbe10af6a50825837408db35b773e133
Marcin <marcinjagielak90@gmail.com>
2023-10-23 00:48:21 +0200

    Add showing student's data

--- src/tests.cpp
+++ src/tests.cpp
@@ -6,27 +6,13 @@ struct DatabaseTest : ::testing::Test {
     Database db;
 };
 
-TEST_F(DatabaseTest, CanAddStudentToDb_Req1_Req2) {
-    Student adam{
-        "Adam",
-        "Kowalski",
-        "ul. Dobra 134, 00-200 Warszawa",
-        123456,
-        "11223344567",
-        Gender::Male};
-
-    EXPECT_TRUE(db.add(adam));
-    EXPECT_FALSE(db.add(adam));
-}
-
 TEST_F(DatabaseTest, DisplayEmptyDb) {
     auto content = db.show();
     auto expected = "";
     EXPECT_EQ(content, expected);
 }
 
-/*TEST(DisplayDb, DisplayNonEmptyDb) {
-    Database db;
+TEST_F(DatabaseTest, DisplayNonEmptyDb) {
     Student adam{
         "Adam",
         "Kowalski",
@@ -35,6 +21,10 @@ TEST_F(DatabaseTest, DisplayEmptyDb) {
         "11223344567",
         Gender::Male
       };
-    EXPECT_TRUE(db.add(adam));
-    db.display();
-}*/
+    db.add(adam);
+    //check adding the same person twice
+    
+    auto content = db.show();
+    auto expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male";
+    EXPECT_EQ(content, expected);
+}
