7156562e10ff1cefc9c6576f41b30881c86a3760
Marcin <marcinjagielak90@gmail.com>
2023-10-22 23:53:32 +0200

    Added Student and Database

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "student.hpp"
+#include "database.hpp"
 
 int main(){
     std::cout << "test" << std::endl;
---
+++ src/student.cpp
@@ -0,0 +1,6 @@
+#include "student.hpp"
+
+Student::Student(std::string name, std::string lastName, std::string address, 
+int indexNumber, std::string pesel, Gender gender) {
+    
+}
