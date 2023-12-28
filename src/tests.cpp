04aaff2254dd737157d371b2482a63051d2b0969
Marcin <marcinjagielak90@gmail.com>
2023-12-28 23:59:47 +0100

    Added tests for sorting with PESEL

--- src/tests.cpp
+++ src/tests.cpp
@@ -12,6 +12,14 @@ struct DatabaseTest : ::testing::Test {
         "11223344567",
         Gender::Male
     };
+    Student janusz{
+        "Janusz",
+        "Tracz",
+        "ul. Dobra 134, 00-200 Warszawa",
+        123456,
+        "11223344565",
+        Gender::Male
+    };
 };
 struct StudentTest : ::testing::Test{
     Student adam{
@@ -31,20 +39,12 @@ TEST_F(DatabaseTest, DisplayEmptyDb) {
 }
 
 TEST_F(DatabaseTest, DisplayNonEmptyDb) {
-    Student janusz{
-        "Janusz",
-        "Tracz",
-        "ul. Dobra 134, 00-200 Warszawa",
-        123456,
-        "11223344567",
-        Gender::Male
-    };
     db.add(adam);
     db.add(janusz);
     //check adding the same person twice
     
     auto content = db.show();
-    auto expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male\nJanusz Tracz; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male\n";
+    auto expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male\nJanusz Tracz; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344565; Male\n";
     EXPECT_EQ(content, expected);
 }
 
@@ -79,5 +79,14 @@ TEST_F(DatabaseTest, CheckStudentByPeselFail){
     db.add(adam);
     auto content = db.findByName("Tracz");
     auto expected = "Nie ma takiego studenta";
+    EXPECT_EQ(content, expected);
+}
+
+TEST_F(DatabaseTest, CheckSortingByPesel){
+    db.add(adam);
+    db.add(janusz);
+    db.sortByPesel();
+    auto content = db.show();
+    auto expected = "Janusz Tracz; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344565; Male\nAdam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male\n";
     EXPECT_EQ(content, expected);
 }
